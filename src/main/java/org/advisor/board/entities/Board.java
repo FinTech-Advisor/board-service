580db96bba1857f9bc62094c921c32db0fd3e93b
SilenceTale <chomn55@gmail.com>
2025-01-23 01:05:27 +0900

    docs, 수정
    자잘한 코드 수정

--- src/main/java/org/advisor/board/entities/Board.java
+++ src/main/java/org/advisor/board/entities/Board.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class Board extends BaseMemberEntity implements Serializable {
     @Id
     @Column(length=30)
-    private String bid;
+    private String seq;
 
     @Column(length=90, nullable = false)
     private String name; // 게시판명
@@ -29,7 +29,6 @@ public class Board extends BaseMemberEntity implements Serializable {
     private boolean useEditor;
     private boolean useEditorImage;
     private boolean useAttachFile;
-    private boolean useComment; // 댓글 사용 여부
     private boolean listUnderView; // 글 보기 하단에 글목록 노출 여부
 
     private String locationAfterWriting; // 글 작성후 이동 경로 - list : 목록, view : 글보기
@@ -38,19 +37,15 @@ public class Board extends BaseMemberEntity implements Serializable {
 
     @Enumerated(EnumType.STRING)
     @Column(length=20, nullable = false)
-    private Authority listAuthority;
+    private Authority listAuthority; // 목록 권한
 
     @Enumerated(EnumType.STRING)
     @Column(length=20, nullable = false)
-    private Authority viewAuthority;
+    private Authority viewAuthority; // 보기 권한
 
     @Enumerated(EnumType.STRING)
     @Column(length=20, nullable = false)
-    private Authority writeAuthority;
-
-    @Enumerated(EnumType.STRING)
-    @Column(length=20, nullable = false)
-    private Authority commentAuthority;
+    private Authority writeAuthority; // 게시판 글 쓰기 권한
 
     @Transient
     private List<String> categories;
