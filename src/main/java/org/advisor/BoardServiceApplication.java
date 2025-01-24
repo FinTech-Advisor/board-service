a8e3d2fa014e3b55837e9505453142726b0ed078
SilenceTale <chomn55@gmail.com>
2025-01-24 20:38:10 +0900

    docs

--- src/main/java/org/advisor/BoardServiceApplication.java
+++ src/main/java/org/advisor/BoardServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BoardServiceApplication {
-
+	// 풀리퀘스트를 위한 테스트 주석
 	public static void main(String[] args) {
 		SpringApplication.run(BoardServiceApplication.class, args);
 	}
